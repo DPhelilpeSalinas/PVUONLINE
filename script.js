afae48225a9566f4793a3b57e79197a667c475d4
Salinas D. P. M <55933926+DPhelilpeSalinas@users.noreply.github.com>
2021-11-04 14:29:30 -0300

    IMPLEMENTAÇÃO DE FUNÇÕES JAVASCRITP
    IMPLEMENTANDO A VERIFICAÇÃO DE TIPOS DE ENTRADA E ERROS POSSIVEIS.

--- script.js
+++ script.js
@@ -1,36 +1,243 @@
-function verificar() {
-	var data = new Date()
-	var ano = data.getFullYear()
-	var fano =document.getElementById('txtano')
-	var res = document.querySelector('div#res')
-	if (fano.value.length == 0 || Number(fano.value) > ano) {
-		window.alert('[ERRO] Verifique os dados e tente novamente!')
-} else {
-	var fsex = document.getElementsByName('radsex')
-	var idade = ano - Number(fano.value)
-	var gênero = ''
-	var img = document.createElement('img')
-	img.setAttribute('id', 'foto')
-} if (fsex[0].checked) {
-		gênero = 'Homem'
-} if (idade>=0 && idade <10) {
-			//Criança
-			img.setAttribute('src', 'foto-bebe-m.png')
-		} else if (idade < 21) {
-			//Criança
-			img.setAttribute('src', 'foto-bebe-m.png')
+function horamoeda() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p1phora = document.getElementById("p1-p-hora").value
+  try {
+    if(p1phora == "") throw "digite quantas horas a planta leva para crescer!"
+		
+    if(isNaN(p1phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p1phora = Number(p1phora)
+    if(p1phora < 24) throw "valor muito baixo no campo ( Hora Planta 1)"
+    
+    if(p1phora > 720) throw "valor muito alto no campo ( Hora Planta 1)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p1-p-hora").value=""
+  }
+}
+function horamoeda2() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p2phora = document.getElementById("p2-p-hora").value
+  try {
+    
+    if(isNaN(p2phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p2phora = Number(p2phora)
+    if(p2phora < 24) throw "valor muito baixo no campo ( Hora Planta 2)"
+    
+    if(p2phora > 720) throw "valor muito alto no campo ( Hora Planta 2)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p2-p-hora").value=""
+  }
+}
+function horamoeda3() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p3phora = document.getElementById("p3-p-hora").value
+  try {
+    
 
-		}if (idade>=0 && idade <10) {
-			//Criança
-			img.setAttribute('src', 'foto-bebe-m.png')
+    if(isNaN(p3phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p3phora = Number(p3phora)
+    if(p3phora < 24) throw "valor muito baixo no campo ( Hora Planta 3)"
+    
+    if(p3phora > 720) throw "valor muito alto no campo ( Hora Planta 3)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p3-p-hora").value=""
+  }
+}
 
-				} if (idade>=0 && idade <10) {
-			//Criança
-			img.setAttribute('src', 'foto-bebe-m.png')
+function horamoeda4() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p4phora = document.getElementById("p4-p-hora").value
+  try {
+    
+    if(isNaN(p4phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p4phora = Number(p4phora)
+    if(p4phora < 24) throw "valor muito baixo no campo ( Hora Planta 4)"
+    
+    if(p4phora > 720) throw "valor muito alto no campo ( Hora Planta 4)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p4-p-hora").value=""
+  }
+}
 
-		//BOA NOITE!
-		img.src = 'fotonoite.png'
-		document.body.style.background = '#515154'
-	}
-		}
+function horamoeda5() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p5phora = document.getElementById("p5-p-hora").value
+  try {
+    
 
+    if(isNaN(p5phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p5phora = Number(p5phora)
+    if(p5phora < 24) throw "valor muito baixo no campo ( Hora Planta 5)"
+    
+    if(p5phora > 720) throw "valor muito alto no campo ( Hora Planta 5)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p5-p-hora").value=""
+  }
+}
+
+function horamoeda6() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p6phora = document.getElementById("p6-p-hora").value
+  try {
+    
+
+    if(isNaN(p6phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p6phora = Number(p6phora)
+    if(p6phora < 24) throw "valor muito baixo no campo ( Hora Planta 6)"
+    
+    if(p6phora > 720) throw "valor muito alto no campo ( Hora Planta 6)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p2-p-hora").value=""
+    document.getElementById("p2-p-hora").value=""
+  }
+/**********************************************************************************************************/
+function horamoeda() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p1phora = document.getElementById("p1-p-hora").value
+  try {
+    if(p1phora == "") throw "digite quantas horas a planta leva para crescer!"
+		
+    if(isNaN(p1phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p1phora = Number(p1phora)
+    if(p1phora < 24) throw "valor muito baixo no campo ( Hora Planta 1)"
+    
+    if(p1phora > 720) throw "valor muito alto no campo ( Hora Planta 1)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p1-p-hora").value=""
+  }
+}
+function horamoeda2() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p2phora = document.getElementById("p2-p-hora").value
+  try {
+    
+    if(isNaN(p2phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p2phora = Number(p2phora)
+    if(p2phora < 24) throw "valor muito baixo no campo ( Hora Planta 2)"
+    
+    if(p2phora > 720) throw "valor muito alto no campo ( Hora Planta 2)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p2-p-hora").value=""
+  }
+}
+function horamoeda3() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p3phora = document.getElementById("p3-p-hora").value
+  try {
+    
+
+    if(isNaN(p3phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p3phora = Number(p3phora)
+    if(p3phora < 24) throw "valor muito baixo no campo ( Hora Planta 3)"
+    
+    if(p3phora > 720) throw "valor muito alto no campo ( Hora Planta 3)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p3-p-hora").value=""
+  }
+}
+
+function horamoeda4() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p4phora = document.getElementById("p4-p-hora").value
+  try {
+    
+    if(isNaN(p4phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p4phora = Number(p4phora)
+    if(p4phora < 24) throw "valor muito baixo no campo ( Hora Planta 4)"
+    
+    if(p4phora > 720) throw "valor muito alto no campo ( Hora Planta 4)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p4-p-hora").value=""
+  }
+}
+
+function horamoeda5() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p5phora = document.getElementById("p5-p-hora").value
+  try {
+    
+
+    if(isNaN(p5phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p5phora = Number(p5phora)
+    if(p5phora < 24) throw "valor muito baixo no campo ( Hora Planta 5)"
+    
+    if(p5phora > 720) throw "valor muito alto no campo ( Hora Planta 5)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p5-p-hora").value=""
+  }
+}
+
+function horamoeda6() {
+  const message = document.getElementById("erromes")
+  message.innerHTML = ""
+  let p6phora = document.getElementById("p6-p-hora").value
+  try {
+    
+
+    if(isNaN(p6phora)) throw "não é um Número, a tabela só aceita números!"
+    
+    p6phora = Number(p6phora)
+    if(p6phora < 24) throw "valor muito baixo no campo ( Hora Planta 6)"
+    
+    if(p6phora > 720) throw "valor muito alto no campo ( Hora Planta 6)"
+    
+  }
+  catch(err) {
+    message.innerHTML = "Corriga o erro: " + err
+    document.getElementById("p2-p-hora").value=""
+    document.getElementById("p2-p-hora").value=""
+  }
